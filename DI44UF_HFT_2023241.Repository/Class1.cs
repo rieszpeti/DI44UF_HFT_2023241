e2e83fbef440751fd03a0288d889f138e427b845
rieszpeti <rieszpeti@gmail.com>
2023-10-04 12:55:36 +0200

    Add ef core

--- DI44UF_HFT_2023241.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DI44UF_HFT_2023241.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ DI44UF_HFT_2023241.Repository/CrawlerDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DI44UF_HFT_2023241.Repository
+{
+    public class CrawlerDbContext : DbContext
+    {
+    }
+}
