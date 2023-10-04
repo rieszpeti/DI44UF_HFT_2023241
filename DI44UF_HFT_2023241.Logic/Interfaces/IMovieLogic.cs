6f821feec054b1884f40aa741a5c431cf844fe9f
rieszpeti <rieszpeti@gmail.com>
2023-10-04 15:57:12 +0200

    add new interfaces to logic

--- DI44UF_HFT_2023241.Logic/Interfaces/IMovieLogic.cs
+++ DI44UF_HFT_2023241.Logic/Interfaces/IMovieLogic.cs
@@ -1,6 +1,17 @@
-﻿namespace DI44UF_HFT_2023241.Logic
+﻿using DI44UF_HFT_2023241.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DI44UF_HFT_2023241.Logic
 {
     public interface IMovieLogic
     {
+        void Create(Movie item);
+        void Delete(int id);
+        double? GetAverageRatePerYear(int year);
+        Movie Read(int id);
+        IQueryable<Movie> ReadAll();
+        void Update(Movie item);
+        IEnumerable<MovieLogic.YearInfo> YearStatistics();
     }
 }
--- DI44UF_HFT_2023241.Logic/Interfaces/IRoleLogic.cs
+++ DI44UF_HFT_2023241.Logic/Interfaces/IRoleLogic.cs
@@ -1,6 +1,14 @@
-﻿namespace DI44UF_HFT_2023241.Logic
+﻿using DI44UF_HFT_2023241.Models;
+using System.Linq;
+
+namespace DI44UF_HFT_2023241.Logic
 {
     public interface IRoleLogic
     {
+        void Create(Role item);
+        void Delete(int id);
+        Role Read(int id);
+        IQueryable<Role> ReadAll();
+        void Update(Role item);
     }
 }
