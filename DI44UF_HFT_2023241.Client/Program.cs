915336b4daf5cb55ff3ee6881a3ca0f95b628c0b
rieszpeti <rieszpeti@gmail.com>
2023-10-04 13:25:47 +0200

    init api caller

--- DI44UF_HFT_2023241.Client/Program.cs
+++ DI44UF_HFT_2023241.Client/Program.cs
@@ -7,6 +7,8 @@ namespace DI44UF_HFT_2023241.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //https://github.com/public-apis/public-apis#shopping
         }
     }
 }
