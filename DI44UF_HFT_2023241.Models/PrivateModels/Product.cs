f7c3ed8b12a594078423b3ef2931daf575858d19
rieszpeti <rieszpeti@gmail.com>
2023-10-04 13:33:59 +0200

    change type of product id

--- DI44UF_HFT_2023241.Models/PrivateModels/Product.cs
+++ DI44UF_HFT_2023241.Models/PrivateModels/Product.cs
@@ -7,7 +7,7 @@ namespace DI44UF_HFT_2023241.Models
     /// </summary>
     public class Product
     {
-        public Guid Id { get; init; }
+        public int Id { get; init; }
         public string Name { get; init; }
         public string Description { get; init; }
         public string Size { get; set; }
