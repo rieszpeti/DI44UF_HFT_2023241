5299d38cb8a3320385751a3dfafb75db251aa62e
rieszpeti <rieszpeti@gmail.com>
2023-10-04 23:41:57 +0200

    add controllers

---
+++ DI44UF_HFT_2023241.EndPoint/Controllers/DirectorController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DI44UF_HFT_2023241.Logic;
+using DI44UF_HFT_2023241.Models;
+using System.Collections.Generic;
+
+namespace DI44UF_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DirectorController : ControllerBase
+    {
+        IDirectorLogic logic;
+        public DirectorController(IDirectorLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Director> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Director Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Director value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Director value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
---
+++ DI44UF_HFT_2023241.EndPoint/Controllers/MovieController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DI44UF_HFT_2023241.Logic;
+using DI44UF_HFT_2023241.Models;
+using System.Collections.Generic;
+
+namespace DI44UF_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+
+        IMovieLogic logic;
+
+        public MovieController(IMovieLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Movie Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Movie value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Movie value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
