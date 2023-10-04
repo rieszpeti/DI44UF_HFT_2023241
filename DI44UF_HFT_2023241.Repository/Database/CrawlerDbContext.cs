df6615ba4cbdb7e05b1165385c9e0b37dda07777
rieszpeti <rieszpeti@gmail.com>
2023-10-04 13:03:23 +0200

    add template

--- DI44UF_HFT_2023241.Repository/Database/CrawlerDbContext.cs
+++ DI44UF_HFT_2023241.Repository/Database/CrawlerDbContext.cs
@@ -6,9 +6,9 @@ namespace DI44UF_HFT_2023241.Repository
 {
     public class CrawlerDbContext : DbContext
     {
-        public DbSet<WebPage> Movies { get; set; }
-        public DbSet<CrawledWebPage> Roles { get; set; }
-        public DbSet<Product> Actors { get; set; }
+        public DbSet<WebPage> WebPages { get; set; }
+        public DbSet<CrawledWebPage> CrawledWebPages { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         public CrawlerDbContext()
         {
@@ -23,6 +23,43 @@ namespace DI44UF_HFT_2023241.Repository
                     .UseLazyLoadingProxies()
                     .UseInMemoryDatabase("Crawler");
             }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<Movie>(movie => movie
+            //    .HasOne(movie => movie.Director)
+            //    .WithMany(director => director.Movies)
+            //    .HasForeignKey(movie => movie.DirectorId)
+            //    .OnDelete(DeleteBehavior.Cascade));
+
+            //modelBuilder.Entity<Actor>()
+            //    .HasMany(x => x.Movies)
+            //    .WithMany(x => x.Actors)
+            //    .UsingEntity<Role>(
+            //        x => x.HasOne(x => x.Movie)
+            //            .WithMany().HasForeignKey(x => x.MovieId).OnDelete(DeleteBehavior.Cascade),
+            //        x => x.HasOne(x => x.Actor)
+            //            .WithMany().HasForeignKey(x => x.ActorId).OnDelete(DeleteBehavior.Cascade));
+
+            //modelBuilder.Entity<Role>()
+            //    .HasOne(r => r.Actor)
+            //    .WithMany(actor => actor.Roles)
+            //    .HasForeignKey(r => r.ActorId)
+            //    .OnDelete(DeleteBehavior.Cascade);
+
+            //modelBuilder.Entity<Role>()
+            //    .HasOne(r => r.Movie)
+            //    .WithMany(movie => movie.Roles)
+            //    .HasForeignKey(r => r.MovieId)
+            //    .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<WebPage>().HasData(new WebPage[] { });
+
+            modelBuilder.Entity<CrawledWebPage>().HasData(new CrawledWebPage[] { });
+
+            modelBuilder.Entity<Product>().HasData(new Product[] { });
         }
     }
 }
