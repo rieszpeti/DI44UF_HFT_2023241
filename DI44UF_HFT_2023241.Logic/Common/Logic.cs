dada937c6fb83e5681a7cc8a2866e75cbc8fee99
rieszpeti <rieszpeti@gmail.com>
2023-11-04 23:20:00 +0100

    add try catch and logger

--- DI44UF_HFT_2023241.Logic/Common/Logic.cs
+++ DI44UF_HFT_2023241.Logic/Common/Logic.cs
@@ -1,6 +1,8 @@
 ﻿using DI44UF_HFT_2023241.Repository;
 using Serilog;
 using System.Linq;
+using System;
+using DI44UF_HFT_2023241.Models;
 
 namespace DI44UF_HFT_2023241.Logic
 {
@@ -18,37 +20,77 @@ namespace DI44UF_HFT_2023241.Logic
 
         public void Create(T item)
         {
-            _repo.Create(item);
+            try
+            {
+                _repo.Create(item);
 
-            _logger.Information("{type} entity successfully created", typeof(T).GetGenericArguments().First());
+                _logger.Information("{type} entity successfully created", typeof(T).GetGenericArguments().First());
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("{message} Couldn't create {type}}", ex.Message, typeof(T));
+                throw;
+            }
         }
 
         public void Delete(int id)
         {
-            _repo.DeleteById(id);
+            try
+            {
+                _repo.DeleteById(id);
 
-            _logger.Information("{type} with {id} successfully deleted", typeof(T).GetGenericArguments().First(), id);
+                _logger.Information("{type} with {id} successfully deleted", typeof(T).GetGenericArguments().First(), id);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("{message} Couldn't delete {type} with {id}", ex.Message, typeof(T).GetGenericArguments().First(), id);
+                throw new Exception("Couldn't delete");
+            }
         }
 
         public T Read(int id)
         {
-            _logger.Information("{type} with {id} start read", typeof(T).GetGenericArguments().First(), id);
+            try
+            {
+                _logger.Debug("{type} with {id} start read", typeof(T).GetGenericArguments().First(), id);
 
-            return _repo.ReadById(id);
+                return _repo.ReadById(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("{message} Couldn't read {type} with {id}", ex.Message, typeof(T).GetGenericArguments().First(), id);
+                throw new Exception("Couldn't Read");
+            }
         }
 
         public IQueryable<T> ReadAll()
         {
-            _logger.Information("{type} start read all", typeof(T).GetGenericArguments().First());
+            try
+            {
+                _logger.Information("{type} start read all", typeof(T).GetGenericArguments().First());
 
-            return _repo.ReadAll();
+                return _repo.ReadAll();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("{message} Couldn't read all {type}", ex.Message, typeof(T).GetGenericArguments().First());
+                throw new Exception("Couldn't Read");
+            }
         }
 
         public void Update(T item)
         {
-            _repo.Update(item);
+            try
+            {
+                _repo.Update(item);
 
-            _logger.Information("{type} with successfully created", typeof(T).GetGenericArguments().First());
+                _logger.Information("{type} with successfully created", typeof(T).GetGenericArguments().First());
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("{message} Couldn't update {type} with {id}", ex.Message, typeof(T).GetGenericArguments().First());
+                throw new Exception("Couldn't Read");
+            }
         }
     }
 }
