559b14e2b95a650c26d631267b07d840dcd7c710
SillySharp <rieszpeti@gmail.com>
2024-03-21 10:38:39 +0100

    init projects

---
+++ DI44UF_HFT_2023241_WPF.Client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DI44UF_HFT_2023241_WPF.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
